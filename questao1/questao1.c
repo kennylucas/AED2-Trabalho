857a02c54473cdda04ef39dd55727cb5f2d918b4
kennylucas <keny.lucas@icomp.ufam.edu.br>
2019-03-27 17:50:38 -0400

    calculando o tempo das funcoes

--- questao1/questao1.c
+++ questao1/questao1.c
@@ -5,16 +5,48 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-//#include <time.h>
+#include <time.h>
 #include "questao1.h"
+
+#define TAMANHO 1000000
 
 int main(){
 
+    clock_t inicio, final;
+    int chave;
+    double clockTime;
+    double mediaSequencial, mediaBinaria;
+    mediaSequencial = mediaBinaria = 0;
     tipoVetor *tp = criaVetor();
-
     preencherVetorAleatoriamente(tp, 100);
 
-    for(int i =0; i<100;i++){
-        printf("%d\n", tp->vet[i]);
+    for(int i=0;i<30;i++){
+        chave = rand()%10000000000;
+
+        //Execução busca binaria
+        inicio = clock();
+        buscaSequencial(tp, TAMANHO, chave);
+        final = clock();
+        clockTime = ((double) (final - inicio)) / CLOCKS_PER_SEC;
+        printf("Tempo de esecução da Busca Sequencial %d: %.4lfms\n", i+1, clockTime*100);
+        mediaSequencial += clockTime;
+
+        //execucao busca sequencial
+        inicio = clock();
+        buscaBinaria(tp, TAMANHO, chave);
+        final = clock();
+        clockTime = ((double) (final - inicio)) / CLOCKS_PER_SEC;
+        printf("Tempo de esecução da Busca Binaria %d: %.4lfms\n\n\n", i+1, clockTime*100);
+        mediaBinaria += clockTime;
+
     }
+    mediaBinaria = mediaBinaria/30;
+    mediaSequencial = mediaSequencial/30;
+
+    printf("Media da busca sequencial: %lf\n",mediaSequencial);
+	printf("Media da busca binaria: %lf\n",mediaBinaria);
+
+    /*for(int i =0; i<100;i++){
+        printf("%d\n", tp->vet[i]);
+    }*/
 }
